49df1d1e0d7a15f1b3cef60f0ca7d0fa82079a44
Jason Gilmore <wj@wjgilmore.com>
2014-09-23 16:48:09 -0400

    Created Todolist model method for determining number of incomplete tasks

--- app/Todolist.php
+++ app/Todolist.php
@@ -8,14 +8,36 @@ class Todolist extends Model {
 
 	protected $fillable = ['name', 'description'];
 
+  /**
+   * Each list is owned by a registered user.
+   *
+   */
   public function user()
   {
       return $this->belongsTo('\todoparrot\User');
   }
 
+  /**
+   * Each list can be associated with one or more tasks.
+   *
+   */
   public function tasks()
   {
     return $this->hasMany('\todoparrot\Task');
+  }
+
+  /**
+   * Calculate the number of incomplete tasks
+   *
+   */
+  public function remainingTasks()
+  {
+
+    $completed = $this->tasks()->where('done', '=', 1)->count();
+    $total = $this->tasks()->count();
+
+    return $total - $completed;
+
   }
 
 }
