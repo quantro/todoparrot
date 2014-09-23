07e8756be67b82d89d2fffaa47d103042148d0b0
Jason Gilmore <wj@wjgilmore.com>
2014-09-23 15:51:38 -0400

    Removed the contact link from the layout navbar as not yet implemented

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,9 +35,8 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="/">Home</a></li>
-        <li><a href="/lists">Lists</a></li>
+        <li><a href="/lists">Your Lists</a></li>
         <li><a href="/about">About</a></li>
-        <li><a href="/contact">Get in Touch</a></li>
           @if(!Auth::check()) 
             <li>{!! HTML::link('users/create', 'Create Account') !!}</li>
             <li>{!! HTML::link('/login', 'Sign In') !!}</li>
