5693eb60d3c8081a486d59f175c30c1dcf9e1a56
Jason Gilmore <wj@wjgilmore.com>
2014-10-02 12:19:33 -0400

    Added reference to TodolistTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder {
 
@@ -11,9 +12,10 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Eloquent::unguard();
+		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+    $this->call('\todoparrot\TodolistTableSeeder');
+
 	}
 
 }
